4a53f1301b9a0c40cc2edcbda53b4771a3f526f1
tyz <tyz@1token.trade>
2022-01-20 18:04:07 +0800

    Update build_all.py

--- build_all.py
+++ build_all.py
@@ -1,5 +1,5 @@
 import os
 
-for folder in ['play', 'clash']:
+for folder in ['play', 'clash', 'ss-server']:
 	os.system(f'cd {folder} && docker build . -t qbtradepub/{folder}')
 	os.system(f'docker push qbtradepub/{folder}')
